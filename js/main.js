61b44209c2ad1df86dec622d592cb4c5eebaa940
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-01-30 09:10:38 +0100

    essais : afficher couleurs des carrés en console.log

--- js/main.js
+++ js/main.js
@@ -9,8 +9,22 @@ let carre2Style = window.getComputedStyle(carre2);
 let carre3Style = window.getComputedStyle(carre3);
 let carre4Style = window.getComputedStyle(carre4);
 
-function afficherCouleur() {
-    console.log(carre1Style.backgroundColor);
+function afficherCouleur(carreStyle) {
+    console.log(carreStyle.backgroundColor);
 }
 
-carre1.addEventListener('click', afficherCouleur);
+carre1.addEventListener('click', function() {
+    afficherCouleur(carre1Style)
+});
+
+carre2.addEventListener('click', function() {
+    afficherCouleur(carre2Style)
+});
+
+carre3.addEventListener('click', function() {
+    afficherCouleur(carre3Style)
+});
+
+carre4.addEventListener('click', function() {
+    afficherCouleur(carre4Style)
+});

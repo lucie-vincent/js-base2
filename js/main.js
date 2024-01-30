cd1f3ba17662816fd020d8c02a802f5b50ddce74
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-01-30 09:45:13 +0100

    ajout texte +  afficher texte on click + essai changer texte et couleur

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
-let carre1 = document.querySelector("#carre1");
-let carre2 = document.querySelector("#carre2");
-let carre3 = document.querySelector("#carre3");
-let carre4 = document.querySelector("#carre4");
-let carre5 = document.querySelector("#carre5");
+let carre1      = document.querySelector("#carre1");
+let carre2      = document.querySelector("#carre2");
+let carre3      = document.querySelector("#carre3");
+let carre4      = document.querySelector("#carre4");
+let carre5      = document.querySelector("#carre5");
+let codeCouleur = document.querySelector(".codeCouleur");
 
 let carre1Style = window.getComputedStyle(carre1);
 let carre2Style = window.getComputedStyle(carre2);
 let carre3Style = window.getComputedStyle(carre3);
 let carre4Style = window.getComputedStyle(carre4);
+let carre5Style = window.getComputedStyle(carre5);
 
 function afficherCouleur(carreStyle) {
-    console.log(carreStyle.backgroundColor);
+    // afficher le texte + changer contenu texte
+    codeCouleur.style.visibility = "visible";
+    codeCouleur.value = `${carreStyle.backgroundColor}`;
+
+    // essai changer couleur
+    // carre5Style.style.backgroundColor = carreStyle.style.backgroundColor;
 }
 
 carre1.addEventListener('click', function() {
@@ -28,3 +35,4 @@ carre3.addEventListener('click', function() {
 carre4.addEventListener('click', function() {
     afficherCouleur(carre4Style)
 });
+

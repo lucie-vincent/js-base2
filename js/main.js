d05bbb1a351ab43f538d78f98076ae4886e8a31c
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-01-31 11:10:18 +0100

    afficher code couleur + changer couleur - fini

--- js/main.js
+++ js/main.js
@@ -1,46 +1,43 @@
-let carre1      = document.querySelector("#carre1");
-let carre2      = document.querySelector("#carre2");
-let carre3      = document.querySelector("#carre3");
-let carre4      = document.querySelector("#carre4");
-let carre5      = document.querySelector("#carre5");
+// let carre2      = document.querySelector("#carre2");
+// let carre3      = document.querySelector("#carre3");
+// let carre4      = document.querySelector("#carre4");
+// let carre5      = document.querySelector("#carre5");
+// let carre2Style = window.getComputedStyle(carre2);
+// let carre3Style = window.getComputedStyle(carre3);
+// let carre4Style = window.getComputedStyle(carre4);
+// let carre5Style = window.getComputedStyle(carre5);
+
+let carres      = document.querySelectorAll(".carre");
 let codeCouleur = document.querySelector(".codeCouleur");
-
-let carre1Style = window.getComputedStyle(carre1);
-let carre2Style = window.getComputedStyle(carre2);
-let carre3Style = window.getComputedStyle(carre3);
-let carre4Style = window.getComputedStyle(carre4);
-let carre5Style = window.getComputedStyle(carre5);
+console.log(carres.length)
 
 
+
+for (var carreIndex  in carres) {
+    const carre = carres[carreIndex]
+
+    console.log(carre)
+    carre.addEventListener('click', function() {
+            afficherCodeCouleur(window.getComputedStyle(carre));
+            changerCouleur(window.getComputedStyle(carre))
+        })
+}
 
 function afficherCodeCouleur(carreStyle) {
+    console.log(carreStyle.backgroundColor)
     codeCouleur.style.visibility = "visible";
     codeCouleur.textContent = `${carreStyle.backgroundColor}`;
+
 }
 
 function changerCouleur(carreStyle) {
-    carre5Style.style.backgroundColor = carreStyle.style.backgroundColor;
+    console.log(carres[carres.length])
+    carres[carres.length-1].style.backgroundColor =carreStyle.backgroundColor;
 }
 
 
-
-carre1.addEventListener('click', () => {
-    afficherCodeCouleur(carre1Style)
-    changerCouleur(carre1Style)
-});
-
-carre2.addEventListener('click', function() {
-    afficherCodeCouleur(carre2Style)
-    changerCouleur(carre2Style)
-});
-
-carre3.addEventListener('click', function() {
-    afficherCodeCouleur(carre3Style)
-    changerCouleur(carre3Style)
-});
-
-carre4.addEventListener('click', function() {
-    afficherCodeCouleur(carre4Style)
-    changerCouleur(carre4Style)
-});
-
+// carre1.addEventListener('click', () => {
+//     console.log(window.getComputedStyle(carre1))
+//     afficherCodeCouleur(window.getComputedStyle(carre1)),
+//     carre5.style.backgroundColor = colorGreen;
+// });
